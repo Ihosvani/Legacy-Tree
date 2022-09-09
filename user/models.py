b8cc4b17ae238893e79dd2a1d3ca83495ecad2b3
Ihosvani <ihosvani2014@gmail.com>
2022-09-08 21:03:03 -0400

    Created user model and serializer

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password_hash = models.CharField()
+    activated = models.BooleanField(default=False)
+
+    class Role(models.TextChoices):
+        ADMIN = 'admin', _('admin')
+        GUEST = 'guest', _('guest')
+        USER = 'user', _('user')
+    
+    role = models.CharField(choices=Role.choices, default=Role.USER)
